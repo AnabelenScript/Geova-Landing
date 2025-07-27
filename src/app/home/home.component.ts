cec28f234995028b5681037f9bfd9e9ddc245295
AnabelenScript <ana0507belen@gmail.com>
2025-07-27 04:41:10 -0600

    Landing terminada, incluyendo api de traducción

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  initGoogleTranslate() {
+  if (!document.getElementById('google-translate-script')) {
+    const script = document.createElement('script');
+    script.id = 'google-translate-script';
+    script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+    document.body.appendChild(script);
+  }
+}
 }
